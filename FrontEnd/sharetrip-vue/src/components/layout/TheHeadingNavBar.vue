c1a37f41d9c0b0d9afee833423930d089fcbc2e1
ChaNyeok1225 <cksgur5530@gmail.com>
2023-11-21 15:17:35 +0900

    fix : 구쿤 where 절 수정

--- FrontEnd/sharetrip-vue/src/components/layout/TheHeadingNavBar.vue
+++ FrontEnd/sharetrip-vue/src/components/layout/TheHeadingNavBar.vue
@@ -1,25 +1,7 @@
 <script setup></script>
 
 <template>
-	<div id="navBar">
-		<router-link :to="{ name: 'main' }">Logo</router-link>
-
-		<router-link :to="{ name: 'baord' }">게시판</router-link>
-		<router-link :to="{ name: 'main' }">관광지</router-link>
-		<router-link :to="{ name: 'main' }">마이페이지</router-link>
-		<a-button type="primary" @click="showDrawer">Open</a-button>
-		<a-drawer
-			title="Basic Drawer"
-			placement="right"
-			:closable="false"
-			v-model:visible="visible"
-			:after-visible-change="afterVisibleChange"
-		>
-			<p>Some contents...</p>
-			<p>Some contents...</p>
-			<p>Some contents...</p>
-		</a-drawer>
-	</div>
+	<div id="navBar">네브바</div>
 </template>
 
 <style scoped></style>
