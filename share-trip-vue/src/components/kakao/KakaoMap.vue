16692c2db897d2667c827eff6612f2b2646088de
오은아 <erropp89@naver.com>
2023-11-21 11:34:08 +0900

    feat 관광지 검색기능

--- share-trip-vue/src/components/kakao/KakaoMap.vue
+++ share-trip-vue/src/components/kakao/KakaoMap.vue
@@ -1,38 +1,90 @@
 <script setup>
+import { onMounted, ref } from "vue";
 
-import { onMounted } from 'vue';
-
-let map= null;
-const initMap= function() {
-    const container= document.getElementById('map');
+let map = null;
+const initMap = function () {
+    const container = document.getElementById("map");
     const options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
-    level: 3,
-}; //지도 객체 등록
-map= new kakao.maps.Map(container, options);
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 5,
+    }; //지도 객체 등록
+    map = new kakao.maps.Map(container, options);
+    //지도에 여러개의 마커를 띄움
+    // displayMarkers();
 };
-
-
 
 onMounted(() => {
     if (window.kakao && window.kakao.maps) {
-      initMap();
+        initMap();
     } else {
-      const script = document.createElement("script");
-      script.src =
-        `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${ //autoload= false 스크립트를 동적으로 로드하기 위해 필요
+        const script = document.createElement("script");
+        script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${
+            //autoload= false 스크립트를 동적으로 로드하기 위해 필요
             import.meta.env.VITE_KAKAO_API_KEY
         }`;
         /* global kakao */
-        script.addEventListener('load', ()=>{
+        script.addEventListener("load", () => {
             kakao.maps.load(initMap);
-        }); //헤드태그에 추가 
-      document.head.appendChild(script);
+        }); //헤드태그에 추가
+        document.head.appendChild(script);
     }
-  });
-  
+});
 
+// //현재 내 위치 기준으로 마커띄우기
+let myCenter = new kakao.maps.LatLng(33.450701, 126.570667);
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        myCenter = new kakao.maps.LatLng(lat, lon);
+        new kakao.maps.Marker({
+            map,
+            position: myCenter,
+        });
+        map.setCenter(myCenter);
+    });
+}
 
+// // 마커를 표시할 위치와 title 객체 배열입니다
+var positions = [
+    {
+        title: "카카오",
+        latlng: new kakao.maps.LatLng(33.450705, 126.570677),
+    },
+    {
+        title: "생태연못",
+        latlng: new kakao.maps.LatLng(33.450936, 126.569477),
+    },
+    {
+        title: "텃밭",
+        latlng: new kakao.maps.LatLng(33.450879, 126.56994),
+    },
+    {
+        title: "근린공원",
+        latlng: new kakao.maps.LatLng(33.451393, 126.570738),
+    },
+];
+
+// // 마커 이미지의 이미지 주소입니다
+var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+const displayMarkers = function () {
+    for (var i = 0; i < positions.length; i++) {
+        // 마커 이미지의 이미지 크기 입니다
+        let imageSize = new kakao.maps.Size(24, 35);
+
+        // 마커 이미지를 생성합니다
+        let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+        // 마커를 생성합니다
+        let marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: positions[i].latlng, // 마커를 표시할 위치
+            title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image: markerImage, // 마커 이미지
+        });
+    }
+};
 </script>
 
 <template>
@@ -42,8 +94,8 @@ onMounted(() => {
 </template>
 
 <style scoped>
-#map{
-    width:500px;
-    height:400px;
+#map {
+    width: 500px;
+    height: 400px;
 }
 </style>
--- share-trip-vue/src/views/TheAttractionView.vue
+++ share-trip-vue/src/views/TheAttractionView.vue
@@ -1,6 +1,106 @@
 <script setup>
-import KakaoMap from '../components/kakao/KakaoMap.vue';
+import KakaoMap from "../components/kakao/KakaoMap.vue";
+import VSelect from "@/components/common/VSelect.vue";
+import { ref, onMounted } from "vue";
+import axios from "axios";
 
+var selectedSido = ref("");
+var selectedGugun = ref("");
+var searchTitle = ref("");
+
+const sidoList = ref([]);
+const gugunList = ref([]);
+const contentTypeId = ref([]);
+
+onMounted(() => {
+    listSido();
+});
+
+const listSido = () => {
+    //시 정보를 가져오는 리스트
+    axios
+        .get("http://192.168.31.55:80/sharetrip/map/sido")
+        .then(function (data) {
+            data = data.data.data;
+            console.log(data);
+
+            let options = [];
+            options.push({ text: "지역", value: "" });
+            options.push({ text: "전체", value: "0" });
+            data.forEach((sido) => {
+                options.push({ text: sido.sidoName, value: sido.sidoCode });
+            });
+            sidoList.value = options;
+        })
+        .catch(function (error) {
+            console.log("시 가져오기 실패");
+        });
+};
+
+const listGugun = (params) => {
+    //군 정보를 가져오는 리스트
+    axios
+        .get("http://192.168.31.55:80/sharetrip/map/gugun", { params })
+        .then(function (data) {
+            data = data.data.data;
+            console.log(data);
+
+            let options = [];
+            options.push({ text: "구군선택", value: "" });
+            data.forEach((gugun) => {
+                options.push({ text: gugun.gugunName, value: gugun.gugunCode });
+            });
+            gugunList.value = options;
+        })
+        .catch(function (err) {
+            console.log("군 가져오기 실패");
+        });
+};
+
+const onChangeSido = (key) => {
+    //시가 바뀌었을 때 호출됨
+    selectedSido.value = key;
+    listGugun({ sido: key });
+};
+
+const onChangeGugun = (key) => {
+    //시가 바뀌었을 때 호출됨
+    selectedGugun.value = key;
+};
+
+const search = () => {
+    let url = new URL("http://192.168.31.55:80/sharetrip/map/attr");
+    const params = new URLSearchParams();
+
+    if (selectedSido.value !== "") {
+        params.append("sido", selectedSido.value);
+    }
+
+    if (selectedGugun.value !== "") {
+        params.append("gugun", selectedGugun.value);
+    }
+
+    if (contentTypeId.value.length > 0) {
+        contentTypeId.value.forEach((id) => {
+            params.append("contentTypeId", id);
+        });
+    }
+
+    if (searchTitle.value !== "") {
+        params.append("keyword", searchTitle.value);
+    }
+
+    url.search = params.toString();
+
+    axios
+        .get(url.toString())
+        .then(function (data) {
+            console.log(data);
+        })
+        .catch(function (error) {
+            console.log("검색 실패");
+        });
+};
 </script>
 
 <template>
@@ -8,10 +108,35 @@ import KakaoMap from '../components/kakao/KakaoMap.vue';
         <h2>관광지 조회</h2>
         <div>지역 테마 검색</div>
 
+        <div>
+            <VSelect :selectOption="sidoList" @onKeySelect="onChangeSido" />
+
+            <div>
+                <VSelect :selectOption="gugunList" @onKeySelect="onChangeGugun" />
+            </div>
+
+            <input v-model="searchTitle" />
+            <button @click="search()">검색</button>
+        </div>
+        <input type="checkbox" id="contentId12" value="12" v-model="contentTypeId" />
+        <label for="contentId12">관광지</label>
+        <input type="checkbox" id="contentId14" value="14" v-model="contentTypeId" />
+        <label for="contentId14">문화시설</label>
+        <input type="checkbox" id="contentId15" value="15" v-model="contentTypeId" />
+        <label for="contentId15">축제공연행사</label>
+        <input type="checkbox" id="contentId25" value="25" v-model="contentTypeId" />
+        <label for="contentId25">여행코스</label>
+        <input type="checkbox" id="contentId28" value="28" v-model="contentTypeId" />
+        <label for="contentId28">레포츠</label>
+        <input type="checkbox" id="contentId32" value="32" v-model="contentTypeId" />
+        <label for="contentId32">숙박</label>
+        <input type="checkbox" id="contentId38" value="38" v-model="contentTypeId" />
+        <label for="contentId38">쇼핑</label>
+        <input type="checkbox" id="contentId39" value="39" v-model="contentTypeId" />
+        <label for="contentId39">음식점</label>
+
         <KakaoMap></KakaoMap>
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
