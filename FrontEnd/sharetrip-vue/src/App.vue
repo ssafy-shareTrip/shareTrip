3f7326d406be7c7aae3e9765a7c7e02a0077d7fd
ChaNyeok1225 <cksgur5530@gmail.com>
2023-11-21 16:27:37 +0900

    add : TheHeadingNavBar

--- FrontEnd/sharetrip-vue/src/App.vue
+++ FrontEnd/sharetrip-vue/src/App.vue
@@ -1,21 +1,22 @@
 <script setup>
-import { ref } from "vue";
 import TheHeadingNavBar from "./components/layout/TheHeadingNavBar.vue";
-
-const isLike = ref(false);
 </script>
 
 <template>
-	<div>
-		<the-heading-nav-bar></the-heading-nav-bar>
+	<the-heading-nav-bar></the-heading-nav-bar>
 
-		<img :src="`/public/icon/like_${isLike}.png`" alt="" @click="isLike = !isLike" />
-		<router-view></router-view>
-	</div>
+	<!-- <router-view></router-view> -->
 </template>
 
-<style scope>
-img:hover {
-	cursor: pointer;
+<style>
+a {
+	text-decoration: none;
+	color: inherit;
+}
+html {
+	min-height: 100%;
+}
+body {
+	min-height: 100vh;
 }
 </style>
--- FrontEnd/sharetrip-vue/src/components/layout/TheHeadingNavBar.vue
+++ FrontEnd/sharetrip-vue/src/components/layout/TheHeadingNavBar.vue
@@ -1,7 +1,24 @@
 <script setup></script>
 
 <template>
-	<div id="navBar">네브바</div>
+	<div class="navBar">
+		<router-link :to="{ name: 'main' }" class="navItem">LOGO</router-link>
+		<router-link :to="{ name: 'board' }" class="navItem">게시판</router-link>
+		<router-link :to="{ name: 'attr' }" class="navItem">관광지</router-link>
+		<router-link :to="{ name: 'mypage' }" class="navItem">마이페이지</router-link>
+	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.navBar {
+	display: flex;
+	width: 100%;
+	height: 50px;
+	line-height: 50px;
+	background-color: rgb(218, 239, 255);
+}
+
+.navItem {
+	margin: 0 2%;
+}
+</style>
