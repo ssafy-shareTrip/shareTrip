2b5f07d1cc3ab961e19d372308eb1198f7c20340
ChaNyeok1225 <cksgur5530@gmail.com>
2023-11-21 17:53:50 +0900

    feat : v-icon 추가

--- FrontEnd/sharetrip-vue/src/components/layout/TheHeadingNavBar.vue
+++ FrontEnd/sharetrip-vue/src/components/layout/TheHeadingNavBar.vue
@@ -11,34 +11,37 @@ const navToggle = ref(false);
 </script>
 
 <template>
-	<v-app-bar title="ShareTrip" class="nav">
-		<router-link :to="{ name: 'board' }" class="navItem">게시판</router-link>
-		<router-link :to="{ name: 'attr' }" class="navItem">관광지</router-link>
-		<router-link :to="{ name: 'mypage' }" class="navItem">마이페이지</router-link>
-		<span @click="navToggle = !navToggle">유저이미지</span>
-	</v-app-bar>
+	<div>
+		<v-app-bar title="ShareTrip" class="nav">
+			<router-link :to="{ name: 'board' }" class="navItem">게시판</router-link>
+			<router-link :to="{ name: 'attr' }" class="navItem">관광지</router-link>
+			<router-link :to="{ name: 'mypage' }" class="navItem">마이페이지</router-link>
+			<span @click="navToggle = !navToggle">유저이미지</span>
+		</v-app-bar>
 
-	<v-navigation-drawer location="right" v-model="navToggle" temporary>
-		<v-list density="compact" nav>
-			<v-list-item
-				><v-btn variant="plain" @click="mvPage('mypage')" class="dBtn"
-					>회원프로필이미지</v-btn
-				></v-list-item
-			>
-			<v-divider></v-divider>
-			<v-list-item
-				><v-btn variant="plain" @click="mvPage('main')" class="dBtn"
-					>로그인</v-btn
-				></v-list-item
-			>
+		<v-navigation-drawer location="right" v-model="navToggle" temporary>
+			<v-list density="compact" nav>
+				<v-list-item
+					><v-btn variant="plain" @click="mvPage('mypage')" class="dBtn"
+						>회원프로필이미지</v-btn
+					></v-list-item
+				>
+				<v-divider></v-divider>
+				<v-list-item
+					><v-btn variant="plain" @click="mvPage('main')" class="dBtn"
+						><v-icon color="success" icon="mdi-antenna" size="large"></v-icon>
+						로그인</v-btn
+					></v-list-item
+				>
 
-			<v-list-item>
-				<v-btn variant="plain" @click="mvPage('main')" class="dBtn">
-					회원가입
-				</v-btn></v-list-item
-			>
-		</v-list>
-	</v-navigation-drawer>
+				<v-list-item>
+					<v-btn variant="plain" @click="mvPage('main')" class="dBtn">
+						회원가입
+					</v-btn></v-list-item
+				>
+			</v-list>
+		</v-navigation-drawer>
+	</div>
 </template>
 
 <style scoped>
--- FrontEnd/sharetrip-vue/src/main.js
+++ FrontEnd/sharetrip-vue/src/main.js
@@ -8,12 +8,21 @@ import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 // Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
 
 const vuetify = createVuetify({
 	components,
 	directives,
+	icons: {
+		defaultSet: "mdi",
+		aliases,
+		sets: {
+			mdi,
+		},
+	},
 });
 const app = createApp(App);
 const pinia = createPinia();
